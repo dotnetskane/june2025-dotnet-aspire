e6c655287e9a891b59ebbd23cd304c6a3eff978a
Peter Nylander <peter.nylander@gmail.com>
2025-06-10 19:03:48 +0200

    Demo setup

--- src/AspireStarter.AppHost/Program.cs
+++ src/AspireStarter.AppHost/Program.cs
@@ -1,25 +1,137 @@
+// 0
 var builder = DistributedApplication.CreateBuilder(args);
 
-var storage = builder.AddAzureStorage("storage")
-                     .RunAsEmulator(
-                     azurite =>
-                     {
-                         azurite.WithLifetime(ContainerLifetime.Persistent);
-                     });
-
-var blobStorage = storage.AddBlobs("blobs");
-var tableStorage = storage.AddTables("tables");
-
-var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiservice")
-    .WithReference(blobStorage)
-    .WithReference(tableStorage)
-    .WaitFor(tableStorage)
-    .WithEnvironment("MY_ENVIRONMENT_VARIABLE", "HELLO_DOTNET_SKANE")
-    .WithEnvironment("TABLE_STORAGE_CONNECTION_STRING", () => storage.GetEndpoint("table").Url);
-
-builder.AddProject<Projects.AspireStarter_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService)
-    .WithEnvironment("MY_ENVIRONMENT_VARIABLE", apiService.GetEndpoint("http"));
-
 builder.Build().Run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 1
+//var builder = DistributedApplication.CreateBuilder(args);
+
+//var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiservice");
+
+//builder.Build().Run();
+
+// 2
+//var builder = DistributedApplication.CreateBuilder(args);
+
+//var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiservice");
+
+//builder.AddProject<Projects.AspireStarter_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(apiService);
+
+//builder.Build().Run();
+
+// 3
+//var builder = DistributedApplication.CreateBuilder(args);
+
+//var storage = builder.AddAzureStorage("storage")
+//                     .RunAsEmulator();
+
+//var tableStorage = storage.AddTables("tables");
+
+//var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiservice")
+//    .WithReference(tableStorage)
+//    .WaitFor(tableStorage);
+
+//builder.AddProject<Projects.AspireStarter_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(apiService);
+
+//builder.Build().Run();
+
+// 4
+//var builder = DistributedApplication.CreateBuilder(args);
+
+//var storage = builder.AddAzureStorage("storage")
+//                     .RunAsEmulator(
+//                     azurite =>
+//                     {
+//                         azurite.WithLifetime(ContainerLifetime.Persistent);
+//                     });
+
+//var tableStorage = storage.AddTables("tables");
+
+//var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiservice")
+//    .WithReference(tableStorage)
+//    .WaitFor(tableStorage);
+
+//builder.AddProject<Projects.AspireStarter_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(apiService);
+
+//builder.Build().Run();
+
+// 5
+//var builder = DistributedApplication.CreateBuilder(args);
+
+//var storage = builder.AddAzureStorage("storage")
+//                     .RunAsEmulator(
+//                     azurite =>
+//                     {
+//                         azurite.WithLifetime(ContainerLifetime.Persistent);
+//                     });
+
+//var blobStorage = storage.AddBlobs("blobs");
+//var tableStorage = storage.AddTables("tables");
+
+//var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiservice")
+//    .WithReference(blobStorage)
+//    .WithReference(tableStorage)
+//    .WaitFor(tableStorage);
+
+//builder.AddProject<Projects.AspireStarter_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(apiService);
+
+//builder.Build().Run();
+
+// 6
+//var builder = DistributedApplication.CreateBuilder(args);
+
+//var storage = builder.AddAzureStorage("storage")
+//                     .RunAsEmulator(
+//                     azurite =>
+//                     {
+//                         azurite.WithLifetime(ContainerLifetime.Persistent);
+//                     });
+
+//var blobStorage = storage.AddBlobs("blobs");
+//var tableStorage = storage.AddTables("tables");
+
+//var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiservice")
+//    .WithReference(blobStorage)
+//    .WithReference(tableStorage)
+//    .WaitFor(tableStorage)
+//    .WithEnvironment("MY_ENVIRONMENT_VARIABLE", "HELLO_DOTNET_SKANE")
+//    .WithEnvironment("TABLE_STORAGE_CONNECTION_STRING", () => storage.GetEndpoint("table").Url);
+
+//builder.AddProject<Projects.AspireStarter_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(apiService)
+//    .WithEnvironment("MY_ENVIRONMENT_VARIABLE", apiService.GetEndpoint("http"));
+
+//builder.Build().Run();
