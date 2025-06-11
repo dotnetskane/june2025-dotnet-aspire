402944e9cf661f798a7ef9d5b3d3e4011d7dc0f2
Peter Nylander <peter.nylander@gmail.com>
2025-06-11 10:59:33 +0200

    Add Azure Storage

--- src/AspireStarter.AppHost/Program.cs
+++ src/AspireStarter.AppHost/Program.cs
@@ -1,6 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiservice");
+var storage = builder.AddAzureStorage("storage")
+                     .RunAsEmulator(
+                     azurite =>
+                     {
+                         azurite.WithLifetime(ContainerLifetime.Persistent);
+                     });
+
+var tableStorage = storage.AddTables("tables");
+
+var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiservice")
+    .WithReference(tableStorage)
+    .WaitFor(tableStorage)
+;
 
 builder.AddProject<Projects.AspireStarter_Web>("webfrontend")
     .WithExternalHttpEndpoints()
