4e392dc810be4daa67267bd9d1667c8ea0167c49
Peter Nylander <peter.nylander@gmail.com>
2025-06-11 10:53:31 +0200

    No orchestration

--- src/AspireStarter.AppHost/Program.cs
+++ src/AspireStarter.AppHost/Program.cs
@@ -1,25 +1,3 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var storage = builder.AddAzureStorage("storage")
-                     .RunAsEmulator(
-                     azurite =>
-                     {
-                         azurite.WithLifetime(ContainerLifetime.Persistent);
-                     });
-
-var blobStorage = storage.AddBlobs("blobs");
-var tableStorage = storage.AddTables("tables");
-
-var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiservice")
-    .WithReference(blobStorage)
-    .WithReference(tableStorage)
-    .WaitFor(tableStorage)
-    .WithEnvironment("MY_ENVIRONMENT_VARIABLE", "HELLO_DOTNET_SKANE")
-    .WithEnvironment("TABLE_STORAGE_CONNECTION_STRING", () => storage.GetEndpoint("table").Url);
-
-builder.AddProject<Projects.AspireStarter_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService)
-    .WithEnvironment("MY_ENVIRONMENT_VARIABLE", apiService.GetEndpoint("http"));
-
 builder.Build().Run();
