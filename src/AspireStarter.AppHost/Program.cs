ea25e3d6cd4ab04c7a13668a4b1376dacce14663
Peter Nylander <peter.nylander@gmail.com>
2025-09-04 15:56:41 +0200

    Add Web

--- src/AspireStarter.AppHost/Program.cs
+++ src/AspireStarter.AppHost/Program.cs
@@ -2,4 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiservice");
 
+builder.AddProject<Projects.AspireStarter_Web>("webfrontend")
+    .WithReference(apiService);
+
 builder.Build().Run();
